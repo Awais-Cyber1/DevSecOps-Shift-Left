c06b0bdd316b4a82b812800f054329c776c42eed
Awais <AH78644@GMAIL.COM>
2024-12-11 16:35:08 +0000

    updatedecproject

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform-dec-project78644" {
+  bucket = "terraform-dec-project78644"
+  }
+  terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-dec-project78644"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
